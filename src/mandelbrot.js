afbd09a6ecca8b9b5fb4ec6f7d138fefda3b0140
piet <pprotassow@posteo.de>
2022-01-06 01:43:13 +0100

    converge behavior of Re(Z(n+1))

--- src/mandelbrot.js
+++ src/mandelbrot.js
@@ -1,29 +1,20 @@
 /** @format */
 
-function mandelbrot(x, y, xMax, xMin, yMax, yMin, width, height) {
-    const x0 = xMin + (x * (xMax - xMin)) / width
-    const y0 = yMin + (y * (yMax - yMin)) / height
-    const black = { r: 0, g: 0, b: 0, a: 255 }
-    const white = { r: 0, g: 0, b: 0, a: 0 }
-    const red = { r: 255, g: 0, b: 0, a: 255 }
+function mandelbrot(Cr, Ci) {
+    let Zi = 0
+    let Zr = 0
+    let Ti = 0
+    let Tr = 0
+    let n = 0
 
-    if (x0 === 0) return red
-    if (y0 === 0) return red
-
-    let r = x0
-    let i = y0
-
-    for (let n = 0; n < 10000; n += 1) {
-        const rr = r * r
-        const ii = i * i
-        if (rr + ii > 4) {
-            return white
-        }
-        i = 2 * r * i + y0
-        r = rr + ii + x0
+    for (; n < 10; n += 1) {
+        Zi = 2 * Zi * Zr + Ci
+        Zr = Tr - Ti + Cr
+        Tr = Zr * Zr
+        Ti = Zi * Zi
+        console.log('konvergenz |Z_0-10|', Zr)
     }
-
-    return black
+    return [n, Zi, Zr]
 }
 
 export default mandelbrot
