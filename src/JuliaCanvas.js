a008d538648a014f7b4fb910771a3c200dd2780b
piet <pprotassow@posteo.de>
2022-01-09 04:30:40 +0100

    julia set not working yet

---
+++ src/JuliaCanvas.js
@@ -0,0 +1,77 @@
+/** @format */
+
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import juliaSet from './juliaSet'
+import genColor from './genColor'
+import drawPixel from './drawPixel'
+
+function Canvas({ state }) {
+    const CanvasRef = useRef(null)
+    const { width, height } = state
+
+    useEffect(() => {
+        const { xMin, xMax, yMin, yMax, iterMax } = state
+        const canvas = CanvasRef.current
+        const context = canvas.getContext('2d')
+
+        const image = context.createImageData(canvas.width, canvas.height)
+        const { data } = image
+
+        function swapBuffer() {
+            context.putImageData(image, 0, 0)
+        }
+
+        const t1 = new Date()
+
+        const mandelObj = juliaSet(
+            xMin,
+            xMax,
+            yMin,
+            yMax,
+            canvas.width,
+            canvas.height,
+            iterMax,
+        )
+        const { iterations, min, max } = mandelObj
+        const colorTable = genColor(min, max)
+        colorTable[100] = { r: 0, b: 0, g: 0, a: 255 }
+        let index = 0
+        for (let x = 0; x < canvas.width; x += 1) {
+            for (let y = 0; y < canvas.height; y += 1) {
+                drawPixel(
+                    x,
+                    y,
+                    iterations[index++],
+                    colorTable,
+                    data,
+                    canvas.width,
+                )
+            }
+        }
+
+        swapBuffer()
+
+        const t2 = new Date()
+        const dt = t2 - t1
+
+        console.log(`elapsed time = ${dt} ms`)
+    })
+
+    return (
+        <div className="border-2">
+            <canvas
+                ref={CanvasRef}
+                className=" border-gray-500"
+                width={width}
+                height={height}
+            />
+        </div>
+    )
+}
+
+Canvas.propTypes = {
+    state: PropTypes.objectOf(PropTypes.number).isRequired,
+}
+
+export default Canvas
---
+++ src/juliaSet.js
@@ -0,0 +1,51 @@
+/** @format */
+import getPerPixel from './getPerPixel'
+import convertPxToComplex from './converter'
+
+function juliaSet(xMin, xMax, yMin, yMax, width, height, iterMax) {
+    const iterations = new Uint32Array(width * height)
+    const radiusZn = new Float32Array(iterations)
+    let index = 0
+    let max = 0
+    let min = iterMax
+    let maxRad = 0
+    let minRad = 10
+    const perPixel = getPerPixel(xMin, xMax, yMin, yMax, width, height)
+    for (let x = 0; x < width; x += 1) {
+        for (let y = 0; y < height; y += 1) {
+            const [Cr, Ci] = convertPxToComplex(
+                x,
+                y,
+                xMin,
+                xMax,
+                yMin,
+                yMax,
+                width,
+                height,
+                perPixel,
+            )
+            let Zi = Ci
+            let Zr = Cr
+            let Ti = 0
+            let Tr = 0
+            let n = 0
+
+            for (; n < iterMax && Tr + Ti <= 4; n += 1) {
+                Zi = 2 * Zi * Zr + 0
+                Zr = Tr - Ti + 0.7885
+                Tr = Zr * Zr
+                Ti = Zi * Zi
+            }
+            iterations[index++] = n
+            const radius = Math.sqrt(Tr + Ti)
+            radiusZn[index] = radius
+            if (n > max) max = n
+            if (n < min) min = n
+            if (radius > max) maxRad = n
+            if (radius < min) minRad = n
+        }
+    }
+    return { iterations, radiusZn, min, max, minRad, maxRad }
+}
+
+export default juliaSet
